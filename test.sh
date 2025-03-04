dbef7664765ccf1fa2aa4c717070c90ad715740d
alaaalbadry <albadryalaaa@gmail.com>
2025-03-04 04:51:13 +0200

    create .git files and mvn related files

---
+++ test.sh
@@ -0,0 +1,9 @@
+git add pom.xml mvnw mvnw.cmd .mvn/wrapper/maven-wrapper.properties \
+    src/main/java/com/micro/eureka_server/EurekaServerApplication.java \
+    src/test/java/com/micro/eureka_server/EurekaServerApplicationTests.java \
+    src/main/resources/application.yml src/main/resources/application.properties \
+    .gitignore .gitattributes
+
+
+git commit -m "Initial commit: Eureka Server setup"
+git push origin main  # or the branch you're working on
